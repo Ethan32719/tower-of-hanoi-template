14e57fe0cc52067f784703a1161ccb76599e6a6e
Ethan32719 <ee6517@pleasantonusd.net>
2025-03-28 20:51:51 +0000

    i finally finished move method

--- TowerModel.java
+++ TowerModel.java
@@ -1,69 +1,56 @@
+// TowerModel.java
 public class TowerModel {
 
-    // 2d array storing game state
     private IntegerStack[] towers;
-
-    // Final tower height
-    private int towerHeight = 0;
-
-    // Debug metrics
+    private int towerHeight;
     private int printCounter = 0;
     private int moveCounter = 0;
 
-    /* This class implements a model of a tower of Hanoi game.
-
-        |    |    |
-        =    |    |
-       ===   |    |
-      =====  |    |
-     ------------------
-        0    1    2
-
-     Example of a game of height three in the starting position.
-
-    */
-    public TowerModel(int height)
-    {
+    public TowerModel(int height) {
         towerHeight = height;
         towers = new IntegerStack[3];
         towers[0] = new IntegerStack(height);
         towers[1] = new IntegerStack(height);
         towers[2] = new IntegerStack(height);
 
-        for (int i=0; i<height; i++)
-        {
+        // Initialize the first tower with disks in descending order
+        for (int i = 0; i < height; i++) {
             towers[0].push(height - i);
         }
     }
 
-    // get the total number of disks
-    public int height()
-    {
+    // Get the height (number of disks)
+    public int height() {
         return towerHeight;
     }
 
-
     // Move one disk from the source stack to the destination stack.
-    public void move(int source, int destination)
-    {
+    public void move(int source, int destination) {
         System.out.println("Move #" + ++moveCounter + " from " + source + " to " + destination);
-        // TODO!!
+
+        // Check if the move is valid: a larger disk cannot be placed on a smaller one.
+        if (towers[destination].peek() == 0 || towers[source].peek() < towers[destination].peek()) {
+            int disk = towers[source].pop();  // Pop the top disk from the source tower
+            towers[destination].push(disk);  // Push it onto the destination tower
+        } else {
+            System.out.println("Invalid move: Cannot place a larger disk on a smaller one.");
+        }
+
+        print();  // Print the updated state of the towers
     }
 
-    // Helper method to nicely print the current model state.
-    public void print()
-    {
+    // Helper method to print the state of the towers
+    public void print() {
         System.out.print("Print #" + ++printCounter + " Towers of Hanoi\n");
 
-        for (int layer = towerHeight-1; layer >= 0; layer--){
+        // Print the tower state from the top to the bottom
+        for (int layer = towerHeight - 1; layer >= 0; layer--) {
             System.out.print("\n");
             for (int tower = 0; tower < towers.length; tower++) {
                 int value = towers[tower].get(layer);
-                if (value == 0)
-                {
+                if (value == 0) {
                     System.out.print(" |");
-                } else
-                {
+                } else {
                     System.out.print(" " + value);
                 }
             }
@@ -75,14 +62,13 @@ public class TowerModel {
         System.out.println();
     }
 
-    // Test instrumentation
-    public IntegerStack[] getTowers()
-    {
+    // Accessor method to get the towers array
+    public IntegerStack[] getTowers() {
         return towers;
     }
 
-    public int getHeight()
-    {
+    // Accessor method to get the tower height
+    public int getHeight() {
         return towerHeight;
     }
 }
--- TowerSolver.java
+++ TowerSolver.java
@@ -1,22 +1,32 @@
-
+// TowerSolver.java
 public class TowerSolver {
+
     private TowerModel model;
 
-    public TowerSolver()
-    {
+    public TowerSolver() {
         // Nothing to do here
     }
 
-    public void solve(TowerModel model)
-    {
+    // Solve the Tower of Hanoi puzzle using recursion
+    public void solve(TowerModel model) {
         this.model = model;
-        // Call the missing solve method (not this one)
+        solveRecursive(model.height(), 0, 2, 1); // Initial call to solve with source=0, destination=2, and auxiliary=1
     }
 
-    // Create an overloaded solve(...) method
-    // This new method will be recursive (call itself)
-    //
-    // [ solve method here]
-    //
+    // Recursive method to solve the Tower of Hanoi puzzle
+    private void solveRecursive(int n, int source, int destination, int auxiliary) {
+        if (n == 1) {
+            // Base case: Move a single disk from source to destination
+            model.move(source, destination);
+        } else {
+            // Move n-1 disks from source to auxiliary
+            solveRecursive(n - 1, source, auxiliary, destination);
+            
+            // Move the nth disk from source to destination
+            model.move(source, destination);
 
+            // Move n-1 disks from auxiliary to destination
+            solveRecursive(n - 1, auxiliary, destination, source);
+        }
+    }
 }
